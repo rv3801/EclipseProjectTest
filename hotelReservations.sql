b60909b0ff0f50a5a470bfe96334c456c3d6d83e
Roberto Villegas <60374178+rv3801@users.noreply.github.com>
2022-11-19 14:08:39 -0800

    Update hotelReservations.sql
    Changed dates to ints for timestamps, created example Reservation table for testing, organized column orders.

--- hotelReservations.sql
+++ hotelReservations.sql
@@ -22,15 +22,12 @@ CREATE TABLE Guest(
     guest_id int not null,
     guest_password varchar(100) not null,
     guest_email varchar(100) not null,
-    /* constraint fk_guest_reservation foreign key (reservation_id) references Reservation(reservation_id),
-    constraint fk_guest_room foreign key (room_id) references Room(room_id),
-	constraint fk_guest_hotel foreign key (hotel_id) references Hotel(hotel_id),*/
     constraint pk_guest primary key (guest_id));
 
 CREATE TABLE Reservation(
 	reservation_id int not null,
-	check_in date not null,
-	check_out date not null,
+	check_in int(11) not null,
+	check_out int(11) not null,
     hotel_id int not null,
     room_id int not null,
     guest_id int not null,
@@ -41,32 +38,46 @@ CREATE TABLE Reservation(
 	constraint pk_reservation primary key(reservation_id));
  
  
-INSERT INTO Hotel(hotel_name, hotel_id, hotel_location) VALUES ("The Hilton", 1, "dtla");
-INSERT INTO Hotel(hotel_name, hotel_id, hotel_location) VALUES ("The Hilton", 2, "sf");
-INSERT INTO Hotel(hotel_name, hotel_id, hotel_location) VALUES ("The Wynn", 3, "lv");
-INSERT INTO Hotel(hotel_name, hotel_id, hotel_location) VALUES ("The Venetian", 4, "lv");
-
-# SELECT hotel_name FROM Hotel WHERE hotel_id = 1;
+INSERT INTO Hotel(hotel_id, hotel_name, hotel_location) VALUES (1, "The Hilton", "Los Angeles, CA");
+INSERT INTO Hotel(hotel_id, hotel_name, hotel_location) VALUES (2, "The Hilton", "San Francisco, CA");
+INSERT INTO Hotel(hotel_id, hotel_name, hotel_location) VALUES (3, "The Wynn", "Las Vegas, NV");
+INSERT INTO Hotel(hotel_id, hotel_name, hotel_location) VALUES (4, "The Venetian", "Las Vegas, NV");
 
 INSERT INTO Room(room_id, room_type, hotel_id) VALUES (1, "King", 1);
 INSERT INTO Room(room_id, room_type, hotel_id) VALUES (2, "Queen", 1);
 INSERT INTO Room(room_id, room_type, hotel_id) VALUES (3, "King", 1);
+INSERT INTO Room(room_id, room_type, hotel_id) VALUES (4, "King", 1);
+INSERT INTO Room(room_id, room_type, hotel_id) VALUES (5, "King", 1);
 INSERT INTO Room(room_id, room_type, hotel_id) VALUES (1, "Queen", 2);
 INSERT INTO Room(room_id, room_type, hotel_id) VALUES (2, "King", 2);
 
-INSERT INTO Guest(guest_fname, guest_lname, guest_id, guest_password, guest_email) VALUES ("Sally", "Doe", 1, "password", "email");
-INSERT INTO Guest(guest_fname, guest_lname, guest_id, guest_password, guest_email) VALUES ("Joe", "Doe",  2, "password", "email");
-INSERT INTO Guest(guest_fname, guest_lname, guest_id, guest_password, guest_email) VALUES ("Hannah", "Doe", 3, "password", "email");
+INSERT INTO Guest(guest_id, guest_fname, guest_lname, guest_password, guest_email) VALUES (1, "Sally", "Doe", "password", "email");
+INSERT INTO Guest(guest_id, guest_fname, guest_lname, guest_password, guest_email) VALUES (2, "Joe", "Doe", "password", "email");
+INSERT INTO Guest(guest_id, guest_fname, guest_lname, guest_password, guest_email) VALUES (3, "Hannah", "Doe", "password", "email");
 
 # sally staying in Hotel 1, Room 1 (King)
 # joe staying in Hotel 1, Room 1 (King)
 # 9 to 11 should work for King
 # 9 to 12 should not work
 
-INSERT INTO Reservation(reservation_id, check_in, check_out, hotel_id, room_id, guest_id, rewards_points) VALUES (1, "2022-11-06", "2022-11-09", 1, 1, 1, 100);
-INSERT INTO Reservation(reservation_id, check_in, check_out, hotel_id, room_id, guest_id, rewards_points) VALUES (2, "2022-11-11", "2022-11-13", 1, 1, 2, 100);
-#INSERT INTO Reservation(reservation_id, check_in, check_out, hotel_id, room_id, guest_id, rewards_points) VALUES (3, "2022-11-06", "2022-11-07", 1, 1, 1, 100);
+# Room 1
+INSERT INTO Reservation(reservation_id, check_in, check_out, hotel_id, room_id, guest_id, rewards_points) VALUES (1, 1667718000, 1667980800, 1, 1, 1, 100);
+/* 11/6/22 - 11/9/22 */
+INSERT INTO Reservation(reservation_id, check_in, check_out, hotel_id, room_id, guest_id, rewards_points) VALUES (2, 1668153600, 1668326400, 1, 1, 2, 100);
+/* 11/11/22 - 11/13/22 */
+# Room 3
+INSERT INTO Reservation(reservation_id, check_in, check_out, hotel_id, room_id, guest_id, rewards_points) VALUES (3, 1668067200, 1668326400, 1, 3, 1, 100);
+/* 11/10/22 - 11/13/22 */
+# Room 4
+INSERT INTO Reservation(reservation_id, check_in, check_out, hotel_id, room_id, guest_id, rewards_points) VALUES (4, 1667718000, 1667808000, 1, 3, 1, 100);
+/* 11/6/22- 11/7/22 */
+INSERT INTO Reservation(reservation_id, check_in, check_out, hotel_id, room_id, guest_id, rewards_points) VALUES (5, 1667980800, 1668240000, 1, 3, 1, 100);
+/* 11/9/22- 11/12/22 */
+# Room 5
+INSERT INTO Reservation(reservation_id, check_in, check_out, hotel_id, room_id, guest_id, rewards_points) VALUES (6, 1667718000, 1668153600, 1, 3, 1, 100);
+/* 11/6/22- 11/11/22 */
 
+select * from reservation;
 
 /*INNER JOIN Room on Room.hotel_id = Hotel.hotel_id
 INNER JOIN Reservation on Reservation.room_id = Room.room_id
