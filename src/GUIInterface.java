7ae257401b1ba84f6afefa4cfdc2dc0be6fbb00a
sophiasiu <sius@oxy.edu>
2022-11-20 11:38:08 -0800

    resolved merged conflicts

--- src/GUIInterface.java
+++ src/GUIInterface.java
@@ -10,25 +10,29 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 	
-public class GUIInterface implements ActionListener{
-		private static JFrame frame1;
-		private static JFrame frame2;
-		private static JButton loginSubmit;
-		private static JPanel loginPanel;
-		private static JTextField userName;
-		private static JTextField password;
-		private static JLabel userLabel;
-		private static JLabel passwordLabel;
+
+public class GUIInterface{
+	private static JFrame frame1;
+	private static JButton loginSubmit;
+	private static JTextField userNameT;
+	private static JTextField passwordT;
+	private static JLabel userLabel;
+	private static JLabel passwordLabel;
+	private static JLabel loginInvalidLabel;
+	private static JLabel loginMismatchLabel;
+	//private static Frame2 frame2;
+	private static String userName;
 		
+	
+	GUIInterface(){
+		createFrames();
+		createButtons();
+		activateButtons();
+		//setPanels();
+		setLabels();
+		setTextInput();
 		
-		GUIInterface(){
-			createFrames();
-			createButtons();
-			activateButtons();
-			//setPanels();
-			setLabels();
-			setTextInput();
-		}
+	}
 		
 
 		public static void createFrames() {
@@ -43,81 +47,104 @@ public class GUIInterface implements ActionListener{
 			frame1.setLayout(null);
 			frame1.setResizable(false);
 			
-			frame2 = new JFrame("MENU");
-			frame2.setSize(800,400);
-			frame2.getContentPane();
-			frame2.setVisible(false);
-			frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			frame2.setLayout(null);
-			frame2.setResizable(false);
 			
 		}
 		
-		public static void createButtons() {
-			loginSubmit = new JButton("Submit");
-			loginSubmit.setSize(100, 50);
-			frame1.add(loginSubmit);
-			loginSubmit.setLocation(350, 300);
-			
-		}
-		
-		public static void activateButtons() {
-			loginSubmit.addActionListener((ActionEvent e) ->{
-				frame1.setVisible(false);
-				frame2.setVisible(true);
-			});
-		}
-		
-		public static void setTextInput() {
-			userName = new JTextField();
-			password = new JTextField();
-			
-			userName.setBounds(270, 100, 250, 30);
-			password.setBounds(270, 150, 250, 30);
-			
-//			loginPanel.add(userName);
-//			loginPanel.add(password);
-			
-			frame1.add(userName);
-			frame1.add(password);
-			
-			//frame1.add(loginPanel);
-			
-			userName.setVisible(true);
-			password.setVisible(true);
-			
-			
-			//will need to getText later
-		}
 
+	public static void createButtons() {
+		loginSubmit = new JButton("Submit");
+		loginSubmit.setSize(100, 50);
+		frame1.add(loginSubmit);
+		loginSubmit.setLocation(350, 300);
 		
-		public static void setLabels() {
-			userLabel = new JLabel("Username: ");
-			passwordLabel = new JLabel("Password: ");
-			
-			
-			userLabel.setBounds(170, 105, 250, 20);
-			passwordLabel.setBounds(170, 155, 250,20);
-			
-			userLabel.setFont(new Font("Times New Roman", Font.PLAIN, 16));
-			passwordLabel.setFont(new Font("Times New Roman", Font.PLAIN, 16));
-			
-			userLabel.setLayout(null);
-			passwordLabel.setLayout(null);
-			
-			userLabel.setVisible(true);
-			passwordLabel.setVisible(true);
-			
-			frame1.add(userLabel);
-			frame1.add(passwordLabel);
-		}
-		
-		
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
 	}
+	
+	public static void activateButtons() {
+		loginSubmit.addActionListener(new ActionListener (){
+	
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+				boolean login_success = false;
+				String guest_username = userNameT.getText();
+				String guest_password = passwordT.getText();
+				
+				if (guest_username == "" || guest_password == "") {
+					loginInvalidLabel.setVisible(true);
+					return;
+					
+				}
+
+				login_success = myDatabase.checkLogin(guest_username, guest_password);
+				
+				if (!(login_success)) {
+					loginMismatchLabel.setVisible(true);
+					return;
+				}
+				
+				userName = guest_username; 
+				frame1.setVisible(false);
+				//frame2 = new Frame2();
+				//frame2.setVisible();
+			}
+			
+		});
+	}
+	
+	public static void setTextInput() {
+		userNameT = new JTextField();
+		passwordT = new JTextField();
+		
+		userNameT.setBounds(270, 100, 250, 30);
+		passwordT.setBounds(270, 150, 250, 30);
+		
+		frame1.add(userNameT);
+		frame1.add(passwordT);
+		
+		userNameT.setVisible(true);
+		passwordT.setVisible(true);
+		
+		
+		//will need to getText later
+	}
+
+	
+	public static void setLabels() {
+		userLabel = new JLabel("Username: ");
+		passwordLabel = new JLabel("Password: ");
+		loginInvalidLabel = new JLabel("Please enter a valid username/password.");
+		loginMismatchLabel = new JLabel("We couldn't find an account with that username/password.");
+		
+		userLabel.setBounds(170, 105, 250, 20);
+		passwordLabel.setBounds(170, 155, 250,20);
+		loginInvalidLabel.setBounds(250, 50, 250, 30);
+		loginMismatchLabel.setBounds(250, 50, 250, 30);
+		
+		userLabel.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+		passwordLabel.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+		loginInvalidLabel.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+		loginMismatchLabel.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+		
+		userLabel.setLayout(null);
+		passwordLabel.setLayout(null);
+		loginInvalidLabel.setLayout(null);		
+		loginMismatchLabel.setLayout(null);
+		
+		userLabel.setVisible(true);
+		passwordLabel.setVisible(true);
+		loginInvalidLabel.setVisible(true);
+		loginMismatchLabel.setVisible(true);
+		
+		frame1.add(userLabel);
+		frame1.add(passwordLabel);
+		frame1.add(loginInvalidLabel);
+		frame1.add(loginMismatchLabel);
+	}
+	
+
+	
+}
+
 
 
