d95fe158bbf5c6e8ef24fa6fd3afe0b641958429
sophiasiu <sius@oxy.edu>
2022-11-26 23:51:59 -0800

    added labels to ViewPersonalInfo

--- src/ViewPersonalInfo.java
+++ src/ViewPersonalInfo.java
@@ -1,14 +1,22 @@
+import java.awt.Font;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class ViewPersonalInfo {
 	
 	private static JFrame viewInfo;
 	private static JButton returnMenu;
-	
+	private static JLabel name;
+	private static JLabel userId;
+	private static JLabel email;
+	private static JLabel rewards;
+
 	ViewPersonalInfo(){
 		createFrames();
 		createMenuButton();
+		createText();
 	}
 	
 	public static void createFrames() {
@@ -31,6 +39,41 @@ public class ViewPersonalInfo {
 		returnMenu.setLocation(620, 10);
 		
 		viewInfo.add(returnMenu);
+	}
+	
+	public static void createText() {
+		name = new JLabel("Name: ");
+		userId = new JLabel("User ID: ");
+		email = new JLabel("Email: ");
+		rewards = new JLabel("Rewards Points: ");
+
+		
+		name.setBounds(250, 105, 250, 20);
+		userId.setBounds(250, 135, 250, 20);
+		email.setBounds(250, 165, 250, 20);
+		rewards.setBounds(250, 195, 250, 20);
+
+		
+		name.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+		userId.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+		email.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+		rewards.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+
+		name.setLayout(null);
+		userId.setLayout(null);
+		email.setLayout(null);
+		rewards.setLayout(null);
+
+		name.setVisible(true);
+		userId.setVisible(true);
+		email.setVisible(true);
+		rewards.setVisible(true);
+
+		viewInfo.add(name);
+		viewInfo.add(userId);
+		viewInfo.add(email);
+		viewInfo.add(rewards);
+
 	}
 }
 
